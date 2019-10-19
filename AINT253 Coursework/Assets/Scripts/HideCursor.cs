4f1a63a4b96ce4f336844170929bac91ba779225
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-10-19 23:05:50 +0100

    Added prototype player.

---
+++ AINT253 Coursework/Assets/Scripts/HideCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor : MonoBehaviour
+{
+    public bool hideCursor = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hideCursor == true)
+        {
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.visible = true;
+        }
+
+    }
+}
