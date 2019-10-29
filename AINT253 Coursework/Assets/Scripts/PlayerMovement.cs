8b68dc167ddbf32b650f34b74142312eb1f94e53
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-10-29 00:59:39 +0000

    Created a functioning FPS controller.

---
+++ AINT253 Coursework/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame.
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Local movement according to player's rotation.
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        rb.MovePosition(rb.position + (move * speed * Time.deltaTime));
+    }
+}
