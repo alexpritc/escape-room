654892dda2573c7b2b7dbefa1fe0385a18f9ddaf
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-11-15 12:43:43 +0000

    A whole load of nothing much.

---
+++ AINT253 Coursework/Assets/Scripts/ZipZapZop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZipZapZop : MonoBehaviour
+{
+    // When?
+    // - NPC is talking.
+    // - PC is on
+    // - Fuse box puzzle is complete.
+
+    // What?
+    // Spawn 2D plane in front of object on the axis that the player is on,
+    // the rotation depends on the players transform position.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
