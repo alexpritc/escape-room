bbf72093263da35dfb8b96dda144f676857278dd
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-12-09 22:23:24 +0000

    Fixed bug with player walk animation not playing.

--- AINT253 Coursework/Assets/Scripts/PlayerMovement.cs
+++ AINT253 Coursework/Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,6 @@ public class PlayerMovement : MonoBehaviour
 
     public static float speed = 10f;
 
-    private float distanceToGround = 1.2f;
-    private float gravity = -9.85f;
-    private float jumpHeight = 2f;
-
     private Vector3 velocity;
     private Vector3 down;
     private Vector3 move;
@@ -19,10 +15,6 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     private Animator anim;
     private AudioSource audioSource;
-
-    private bool isGrounded;
-
-    private RaycastHit hit;
 
     private Transform playerTransform;
 
@@ -33,22 +25,12 @@ public class PlayerMovement : MonoBehaviour
         audioSource = gameObject.GetComponent<AudioSource>();
 
         playerTransform = transform;
-        isGrounded = true;
     }
 
     // Update is called once per frame.
     // Player input.
     void Update()
     {
-        // Get jump input.
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
-            Debug.Log("Jump!");
-
-            isGrounded = false;
-        }
-
         // Get movement input.
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -69,9 +51,6 @@ public class PlayerMovement : MonoBehaviour
 
         // Local movement according to player's rotation.
         move = playerTransform.right * x + playerTransform.forward * z;
-
-        // Apply gravity to velocity.
-        velocity.y += gravity * Time.deltaTime;
     }
 
     // Physics.
@@ -79,24 +58,5 @@ public class PlayerMovement : MonoBehaviour
     {
         // Play footstep sounds here.
         rb.MovePosition(rb.position + (move * speed * Time.deltaTime));
-
-        // Play jump sound here.
-        rb.MovePosition(rb.position + (velocity * Time.deltaTime));
-        GroundCheck();
-    }
-
-    void GroundCheck()
-    {
-        // Draw raycast downwards to check if the player is touching the ground.
-        down = transform.TransformDirection(Vector3.down);
-
-        Debug.DrawRay(transform.position, down, Color.red);
-
-        if (Physics.Raycast(transform.position, down, out hit, distanceToGround))
-        {
-            Debug.DrawRay(transform.position, down * hit.distance, Color.yellow);
-            isGrounded = true;
-            velocity.y = 0f;
-        }
     }
 }
